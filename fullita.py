97c23a3698b5bdd4c14810ba8a4f161d5e3785d3
Daitan91 <133069748+Daitan91@users.noreply.github.com>
2025-06-01 23:38:09 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://elwood65-vadase.hf.space"
-PASSWORD = "shadow65"
+LINKMFP = "https://daitan91-universe.hf.space"
+PASSWORD = "000"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
