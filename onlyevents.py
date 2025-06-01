28618f6bf00430357330688a824f95054db1862f
Daitan91 <133069748+Daitan91@users.noreply.github.com>
2025-06-01 23:39:00 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://elwood65-vadase.hf.space"
-PASSWORD = "shadow65"
+LINKMFP = "https://daitan91-universe.hf.space"
+PASSWORD = "000"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
